1dd78f71ebc9a9c1b83b305ea737b55f9629781f
BibloBagginz4 <sdsuguard-indeed@yahoo.com>
2025-11-18 03:55:24 -0800

    Initial Upload

---
+++ submissions/playback/playback.py
@@ -0,0 +1,4 @@
+# This program will prompt the user for input and then outputs that same input, replacing each space with three dots (i.e. ...)
+
+user_input = input("Please input a short sentence of your choice: ")
+print(user_input.replace(" ", "..."))
